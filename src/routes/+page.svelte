cc293c0ffcbeda6c425f2a389262a5c3bfa9e277
peko <yoyo.mepa@gmail.com>
2024-01-11 15:05:28 +0100

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1 class="text-3xl font-bold underline text-red-100">
+  Hello world! willmse
+</h1>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
