4148d110572b577b2f84236569fec0f89a64193b
peko <yoyo.mepa@gmail.com>
2024-01-14 11:16:51 +0100

    align right

--- src/lib/components/App.svelte
+++ src/lib/components/App.svelte
@@ -15,19 +15,19 @@
 <div class="relative w-full h-full">
 
     <div class="absolute ">
-        <div class="sm:pt-20 sm:pl-20 pt-10 pl-10 text-right ">
-            <h1 class="font-bold text-orange-600 text-2xl sm:text-7xl">
+        <div class="pt-10 pl-10 text-right sm:pt-20 sm:pl-20 ">
+            <h1 class="text-2xl font-bold text-orange-600 sm:text-7xl">
                 Mepa Yohann.
             </h1>
-            <h2 class="-mt-1 text-normal font-thin text-orange-400 sm:text-2xl">Developper</h2>
+            <h2 class="-mt-1 font-thin text-orange-400 text-normal sm:text-2xl">Developper</h2>
         </div>
     </div>
-    <div class="absolute flex items-end justify-end w-5/6 sm:w-1/2 text-slate-200 sm:bottom-20 sm:right-20 bottom-16 right-5">
+    <div class="absolute flex items-end justify-end w-5/6 text-right sm:w-1/2 text-slate-200 sm:bottom-20 sm:right-20 bottom-16 right-5">
         {#if showMenu}
             <ul>
                 <li class="font-thin hover:text-orange-200 transition ease-in-out" >
-                    <button on:click={()=>disp('work')} class="text-2xl hover:text-orange-400" >
-                        Personal Work.
+                    <button on:click={()=>disp('about')} class="text-2xl hover:text-orange-400" >
+                        About.
                     </button>
                 </li>
                 <li class="font-thin hover:text-orange-200 transition ease-in-out" >
@@ -36,8 +36,8 @@
                     </button>
                 </li>
                 <li class="font-thin hover:text-orange-200 transition ease-in-out" >
-                    <button on:click={()=>disp('about')} class="text-2xl hover:text-orange-400" >
-                        About.
+                    <button on:click={()=>disp('work')} class="text-2xl hover:text-orange-400" >
+                        Personal Work.
                     </button>
                 </li>
             </ul>
@@ -52,14 +52,14 @@
                     {/each}
                 </div>
             {:else if section == 'experience'}
-                <div class="overflow-scroll text-2xl h-96 bg-black bg-opacity-60 rounded-lg px-10">
+                <div class="px-10 overflow-scroll text-2xl bg-black rounded-lg h-96 bg-opacity-60">
                     <div>
                         {#each infos.xps as exp }
                             <div class="pb-8 grid grid-cols-4 gap-2">
                                 <div class="pt-2 mr-2 text-sm text-slate-100">
                                     {exp.time}
                                 </div>
-                                <div class="col-span-3">
+                                <div class="text-left col-span-3">
                                     <p class="font-bold text-slate-100">
                                         {exp.position}
                                         •
@@ -77,9 +77,9 @@
                     </div>  
                 </div>
             {:else if section == 'about'}
-                <div class="sm:ml-10 bg-black bg-opacity-60 rounded-lg px-10">
+                <div class="px-10 bg-black rounded-lg sm:ml-10 bg-opacity-60">
                     <h3 class="text-2xl font-bold">{infos.about.title}</h3>
-                    <p class="pb-5 text-justify text-slate-300 font-light" in:fly={{ delay: 250,duration: 300, x: 10, opacity: 0.5, easing: quintOut }}>{@html infos.about.conent}</p>
+                    <p class="pb-5 font-light text-justify text-slate-300" in:fly={{ delay: 250,duration: 300, x: 10, opacity: 0.5, easing: quintOut }}>{@html infos.about.conent}</p>
                 </div>
             {/if}
         {/if}   
@@ -93,7 +93,7 @@
     {:else }
     <div class="absolute flex overflow-scroll text-slate-400 sm:bottom-10 sm:right-20 bottom-5 right-10">
     {#each infos.links as link }
-        <a class="block italic font-thin hover:underline hover:text-orange-400 text-slate-100 transition ease-in-out mx-2" href={link.link} target="_blank" rel="noopener noreferrer ">
+        <a class="block mx-2 italic font-thin hover:underline hover:text-orange-400 text-slate-100 transition ease-in-out" href={link.link} target="_blank" rel="noopener noreferrer ">
             {@html link.icon} 
         </a>
         
