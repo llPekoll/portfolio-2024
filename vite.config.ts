cc71f39b1fd5f82963dfdffe4874b0ddc895d104
peko <yoyo.mepa@gmail.com>
2024-10-22 23:05:02 +0200

    st

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: './',
   plugins: [sveltekit()],
   ssr: {
     noExternal: ['three']
-  },
-  server: {
-    port: 3001
   }
 })
